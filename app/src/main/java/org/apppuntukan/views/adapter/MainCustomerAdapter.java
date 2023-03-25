c8afb8a2e03c026ec1755890d5b7dc356bff5bbf
pitzzahh <pitzzahh@araopj.tech>
2023-03-25 10:56:41 +0800

    refactor: recycler view adapter constructor params

--- app/src/main/java/org/apppuntukan/views/adapter/MainCustomerAdapter.java
+++ app/src/main/java/org/apppuntukan/views/adapter/MainCustomerAdapter.java
@@ -1,7 +1,5 @@
 package org.apppuntukan.views.adapter;
 
-import java.util.List;
-
 import android.content.Context;
 import android.view.View;
 import org.apppuntukan.R;
@@ -11,21 +9,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import android.view.LayoutInflater;
-import org.apppuntukan.model.Product;
 import org.apppuntukan.viewmodel.ICard;
 import org.apppuntukan.model.ProductService;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainCustomerAdapter extends RecyclerView.Adapter<MainCustomerAdapter.CardHolder> {
 
     private final Context context;
-
-    private final List<Product> products;
-
     private final ICard iCard;
 
-    public MainCustomerAdapter(List<Product> products, Context context, ICard iCard) {
-        this.products = products;
+    public MainCustomerAdapter(Context context, ICard iCard) {
         this.context = context;
         this.iCard = iCard;
     }
@@ -41,24 +35,38 @@ public class MainCustomerAdapter extends RecyclerView.Adapter<MainCustomerAdapte
 
     @Override
     public void onBindViewHolder(@NonNull MainCustomerAdapter.CardHolder holder, int position) {
-
         holder.addToCart.setOnClickListener(view -> {
-            if (ProductService.getInstance().getCartProducts().contains(products.get(position))) {
-                ProductService.getInstance().addProductQuantity(products.get(position));
+            int index = ProductService.getInstance().getProductIndexFromCart(ProductService.getInstance().getProducts().get(position));
+            System.out.println("index = " + index);
+            if (index != -1 &&
+                    ProductService.getInstance().getCartProducts().get(index).getStock() <=
+                            Integer.parseInt(ProductService.getInstance().getProductQuantityInCart(ProductService.getInstance().getCartProducts().get(index)))
+            ) {
+                Snackbar.make(view, "Product out of stock", Snackbar.LENGTH_SHORT)
+                        .show();
             } else {
-                ProductService.getInstance().addProductToCart(products.get(position));
+                if (ProductService.getInstance().isAlreadyInCart(ProductService.getInstance().getProducts().get(position))) {
+                    ProductService.getInstance().addProductQuantity(ProductService.getInstance().getProducts().get(position));
+                } else {
+                    ProductService.getInstance().addProductToCart(ProductService.getInstance().getProducts().get(position));
+                }
+                Snackbar.make(view, "Product added to cart", Snackbar.LENGTH_SHORT)
+                        .show();
             }
         });
-
-        holder.getImageView().setImageResource(R.drawable.product_icon);
-        holder.getProductName().setText(products.get(position).getProductName());
-        holder.getPrice().setText(String.format("$%s", products.get(position).getPrice()));
-        holder.getAvailableStock().setText(String.format("Available stock: %s", products.get(position).getStock()));
+        if (ProductService.getInstance().getProducts().get(position).getStock() >= 1) {
+            holder.getImageView().setImageResource(R.drawable.product_icon);
+        } else {
+            holder.getImageView().setImageResource(R.drawable.no_product_icon);
+        }
+        holder.getProductName().setText(ProductService.getInstance().getProducts().get(position).getProductName());
+        holder.getPrice().setText(String.format("$%s", ProductService.getInstance().getProducts().get(position).getPrice()));
+        holder.getAvailableStock().setText(String.format("Available stock: %s", ProductService.getInstance().getProducts().get(position).getStock()));
     }
 
     @Override
     public int getItemCount() {
-        return products.size();
+        return ProductService.getInstance().getProducts().size();
     }
 
     public static class CardHolder extends RecyclerView.ViewHolder {
