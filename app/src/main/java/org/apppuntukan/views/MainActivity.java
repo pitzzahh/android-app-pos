7d85e970a0d9ef15d43561cffc8b1acdaae8afb1
pitzzahh <pitzzahh@araopj.tech>
2023-03-30 22:55:39 +0800

    chore: added getInstance util method

--- app/src/main/java/org/apppuntukan/views/MainActivity.java
+++ app/src/main/java/org/apppuntukan/views/MainActivity.java
@@ -2,13 +2,10 @@ package org.apppuntukan.views;
 
 import org.apppuntukan.R;
 import android.os.Bundle;
-import android.view.View;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
-import org.apppuntukan.viewmodel.ICard;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.RecyclerView;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import org.apppuntukan.databinding.ActivityMainBinding;
 import org.apppuntukan.views.adapter.MainCustomerAdapter;
@@ -16,7 +13,9 @@ import org.apppuntukan.viewmodel.ProductsActivityViewModel;
 import org.apppuntukan.model.abstractions.NoActionBarActivity;
 import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
 
-public class MainActivity extends NoActionBarActivity implements ICard {
+public class MainActivity extends NoActionBarActivity {
+
+    static MainActivity mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +33,11 @@ public class MainActivity extends NoActionBarActivity implements ICard {
 
     }
 
-    @Override
-    public void onClickCard(View v) { // FIXME: 28/03/2023 Won't work
-        Snackbar.make(v, "Card clicked", Snackbar.LENGTH_SHORT)
-                .show();
-        System.out.println("Clicked card");
-    }
-
-    @Override
-    public void onAddToCart(View v) { // FIXME: 28/03/2023 Won't work
-        Snackbar.make(v, "Added to cart", Snackbar.LENGTH_SHORT)
-                .show();
-        System.out.println("Add to Cart clicked");
+    public static MainActivity getInstance() {
+        if (mainActivity == null) {
+            return new MainActivity();
+        }
+        return mainActivity;
     }
 
 }
