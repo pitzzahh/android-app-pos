c1903dfb19dbb837f076302e8fb343d55138c24e
pitzzahh <pitzzahh@araopj.tech>
2023-03-30 23:05:53 +0800

    chore: add ICard implementation

--- app/src/main/java/org/apppuntukan/views/adapter/MainCustomerAdapter.java
+++ app/src/main/java/org/apppuntukan/views/adapter/MainCustomerAdapter.java
@@ -1,14 +1,17 @@
 package org.apppuntukan.views.adapter;
 
 import org.apppuntukan.R;
+import android.view.View;
 import android.app.Activity;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import org.apppuntukan.model.Product;
+import org.apppuntukan.viewmodel.ICard;
 import org.apppuntukan.model.ProductService;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.snackbar.Snackbar;
 import org.apppuntukan.databinding.ProductCardBinding;
 
 public class MainCustomerAdapter extends RecyclerView.Adapter<MainCustomerAdapter.CardHolder> {
@@ -42,7 +45,7 @@ public class MainCustomerAdapter extends RecyclerView.Adapter<MainCustomerAdapte
         return ProductService.getInstance().getProducts().size();
     }
 
-    public static class CardHolder extends RecyclerView.ViewHolder {
+    public static class CardHolder extends RecyclerView.ViewHolder implements ICard {
 
         private final ImageView imageView;
         private final ProductCardBinding productCardBinding;
@@ -55,12 +58,32 @@ public class MainCustomerAdapter extends RecyclerView.Adapter<MainCustomerAdapte
 
         void bindCard(Product product) {
             productCardBinding.setProduct(product);
+            productCardBinding.setHandler(this);
             productCardBinding.executePendingBindings();
             if (product.getStock() >= 1) {
                 imageView.setImageResource(R.drawable.product_icon);
             } else {
                 imageView.setImageResource(R.drawable.no_product_icon);
             }
+        }
+
+        @Override
+        public void onClickCard(View v) {
+            Snackbar.make(v, "Card clicked", Snackbar.LENGTH_SHORT)
+                    .show();
+        }
+
+        @Override
+        public void onAddToCart(View view) {
+            int pos = super.getLayoutPosition(); // get the position of the card
+            System.out.println("position: " + pos);
+            if (ProductService.getInstance().isAlreadyInCart(ProductService.getInstance().getProducts().get(pos))) {
+                ProductService.getInstance().addProductQuantity(ProductService.getInstance().getProducts().get(pos));
+            } else {
+                ProductService.getInstance().addProductToCart(ProductService.getInstance().getProducts().get(pos));
+            }
+            Snackbar.make(view, "Product added to cart", Snackbar.LENGTH_SHORT)
+                    .show();
         }
     }
 }
