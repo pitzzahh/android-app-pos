865a7ae61a769d2f9da026d147e0431c3f94dacb
pitzzahh <pitzzahh@araopj.tech>
2023-03-31 22:23:44 +0800

    chore: add more ICard abstract method for card onclick events

--- app/src/main/java/org/apppuntukan/viewmodel/ICard.java
+++ app/src/main/java/org/apppuntukan/viewmodel/ICard.java
@@ -8,4 +8,8 @@ public interface ICard {
     default void onClickProductImage(View v) {}
 
     default void onAddToCart(View v) {}
+
+    default void onIncreaseQuantity(View v) {}
+
+    default void onDecreaseQuantity(View v) {}
 }
