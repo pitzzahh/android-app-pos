e470ff18e0bdeb1e3665c705b868b3f6e637ebe4
pitzzahh <pitzzahh@araopj.tech>
2023-04-02 00:01:54 +0800

    chore: add nitrite db connection

--- app/src/main/java/org/apppuntukan/views/MainActivity.java
+++ app/src/main/java/org/apppuntukan/views/MainActivity.java
@@ -4,20 +4,24 @@ import org.apppuntukan.R;
 import android.os.Bundle;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import org.apppuntukan.databinding.ActivityMainBinding;
 import org.apppuntukan.model.ProdServ;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.RecyclerView;
-import org.apppuntukan.databinding.ActivityMainBinding;
 import org.apppuntukan.databinding.ProductCardBinding;
 import com.google.android.flexbox.FlexboxLayoutManager;
 import org.apppuntukan.views.adapter.RecyclerViewAdapter;
 import org.apppuntukan.viewmodel.ProductsActivityViewModel;
 import org.apppuntukan.model.abstractions.NoActionBarActivity;
+import org.dizitart.no2.Nitrite;
 import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
 
 public class MainActivity extends NoActionBarActivity {
 
     static MainActivity mainActivity;
+
+    static Nitrite db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +30,27 @@ public class MainActivity extends NoActionBarActivity {
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setProductsViewModel(new ViewModelProvider(this).get(ProductsActivityViewModel.class));
-        binding.setLifecycleOwner(this);
         setContentView(binding.getRoot());
+
+        db = Nitrite.builder()
+                .compressed()
+                .filePath(getFilesDir().getPath() + "/test.db")
+                .openOrCreate("user", "password");
+
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         RecyclerViewAdapter<ProductCardBinding> adapter = new RecyclerViewAdapter<>(this, R.layout.product_card, ProdServ.instance().getProducts());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new FlexboxLayoutManager(this));
+
+        Runtime.getRuntime()
+                .addShutdownHook(new Thread(() -> getDb().close()));
     }
 
-    public static MainActivity getInstance() {
+    public static synchronized Nitrite getDb() {
+        return db;
+    }
+
+    public static synchronized MainActivity instance() {
         if (mainActivity == null) {
             return new MainActivity();
         }
