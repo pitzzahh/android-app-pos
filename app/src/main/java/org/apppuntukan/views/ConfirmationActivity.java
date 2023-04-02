3f168a54880c2517e93a9380f34f486bae3dac50
pitzzahh <pitzzahh@araopj.tech>
2023-04-02 21:47:41 +0800

    chore: refactor data binding of view model for ConfirmationActivity

--- app/src/main/java/org/apppuntukan/views/ConfirmationActivity.java
+++ app/src/main/java/org/apppuntukan/views/ConfirmationActivity.java
@@ -2,9 +2,7 @@ package org.apppuntukan.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
-import android.widget.TextView;
 import org.apppuntukan.R;
 import org.apppuntukan.databinding.ActivityConfirmationBinding;
 import org.apppuntukan.model.Product;
@@ -18,17 +16,13 @@ public class ConfirmationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityConfirmationBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_confirmation);
-        binding.setConfirmationViewModel(new ViewModelProvider(this).get(ConfirmationActivityViewModel.class));
-
+        binding.setConfirmationViewModel(ConfirmationActivityViewModel.instance());
+        binding.setLifecycleOwner(this);
         setContentView(binding.getRoot());
-
-        TextView price = findViewById(R.id.lbl_price);
-        price.setText(String.format("$%s", ProdServ.instance().getTotal()));
-        TextView change = findViewById(R.id.lbl_change);
-        change.setText(String.format("$%s", ProdServ.instance().getChange()));
 
         List<Product> products = ProdServ.instance().getProducts();
         List<Product> cartProducts = ProdServ.instance().getCartProducts();
+
         for (int i = 0; i < cartProducts.size(); i++) {
             Product product = cartProducts.get(i);
             if (products.get(i).getId() == product.getId()) { // FIXME: 25/03/2023 decrease product stock based on ordered quantity
