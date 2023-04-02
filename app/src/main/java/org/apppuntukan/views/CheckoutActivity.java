741f06ce54ce2c43244bec724d9e1e13e55fb25a
pitzzahh <pitzzahh@araopj.tech>
2023-04-02 22:01:00 +0800

    chore: cleared CheckoutActivity duplicate code

--- app/src/main/java/org/apppuntukan/views/CheckoutActivity.java
+++ app/src/main/java/org/apppuntukan/views/CheckoutActivity.java
@@ -1,15 +1,8 @@
 package org.apppuntukan.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import com.google.android.material.snackbar.Snackbar;
-
 import org.apppuntukan.R;
-import org.apppuntukan.model.ProdServ;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -17,29 +10,5 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
-        Button button = findViewById(R.id.btn_open_confirmation);
-
-        String totalCartPrice = ProdServ.instance().computeTotal();
-
-        button.setOnClickListener(view -> {
-            Intent intent = new Intent(this, ConfirmationActivity.class);
-            String payment = ((EditText)findViewById(R.id.amount_to_pay)).getText().toString();
-            if (payment.matches("\\d|\\d+")) {
-                double total = Double.parseDouble(totalCartPrice);
-                if (Double.parseDouble(payment) >= total) {
-                    ProdServ.instance().setTotal(total);
-                    ProdServ.instance().setChange((Double.parseDouble(payment) - total));
-                    startActivity(intent);
-                } else {
-                    Snackbar.make(view, "Insufficient cash", Snackbar.LENGTH_SHORT).show();
-                }
-            } else {
-                Snackbar.make(view, "Invalid cash", Snackbar.LENGTH_SHORT).show();
-            }
-        });
-
-        TextView textView = findViewById(R.id.label_price);
-        textView.setText(String.format("$%s", totalCartPrice));
-
     }
 }
