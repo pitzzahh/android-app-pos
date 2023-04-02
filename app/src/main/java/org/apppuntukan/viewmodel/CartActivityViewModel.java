c8a0d0f467eca5c073d4010023204a155b292378
pitzzahh <pitzzahh@araopj.tech>
2023-04-02 13:40:24 +0800

    chore: add updateCartProductCount and changed currency symbol tp php

--- app/src/main/java/org/apppuntukan/viewmodel/CartActivityViewModel.java
+++ app/src/main/java/org/apppuntukan/viewmodel/CartActivityViewModel.java
@@ -1,14 +1,10 @@
 package org.apppuntukan.viewmodel;
 
+import java.util.Locale;
 import android.view.View;
 import android.content.Intent;
-
-import androidx.databinding.BaseObservable;
-import androidx.databinding.ObservableField;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-
 import org.apppuntukan.model.ProdServ;
+import androidx.lifecycle.MutableLiveData;
 import org.apppuntukan.views.CheckoutActivity;
 
 public class CartActivityViewModel extends ViewModelBase {
@@ -17,16 +13,23 @@ public class CartActivityViewModel extends ViewModelBase {
 
     public MutableLiveData<String> Total = new MutableLiveData<>();
 
+    public MutableLiveData<String> totalProducts = new MutableLiveData<>();
+
     public CartActivityViewModel() {
         updateCartTotal();
+        updateCartProductCount();
     }
     public void openCheckout(View view) {
         view.getContext()
                 .startActivity(new Intent(view.getContext(), CheckoutActivity.class));
     }
 
+    public void updateCartProductCount() {
+        totalProducts.setValue(String.format(Locale.getDefault(),"Cart(%d)", ProdServ.instance().getCartProducts().size()));
+    }
+
     public void updateCartTotal(){
-        Total.setValue(String.format("Total: $%s", ProdServ.instance().computeTotal()));
+        Total.setValue(String.format("Total: ₱%s", ProdServ.instance().computeTotal()));
     }
 
     public static synchronized CartActivityViewModel getInstance()
