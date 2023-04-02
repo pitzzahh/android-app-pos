74472e1e2627fd6fe531226e5a846b5618abb9c6
pitzzahh <pitzzahh@araopj.tech>
2023-04-02 13:41:45 +0800

    chore: add method for updating cart product count for cart btn

--- app/src/main/java/org/apppuntukan/viewmodel/ProductsActivityViewModel.java
+++ app/src/main/java/org/apppuntukan/viewmodel/ProductsActivityViewModel.java
@@ -1,19 +1,29 @@
 package org.apppuntukan.viewmodel;
 
+import java.util.Locale;
 import android.view.View;
 import android.content.Intent;
+import org.apppuntukan.model.ProdServ;
 import org.apppuntukan.views.CartActivity;
-import androidx.databinding.ObservableField;
+import androidx.lifecycle.MutableLiveData;
 
 public class ProductsActivityViewModel extends ViewModelBase {
 
     static ProductsActivityViewModel model;
 
-    public ObservableField<Integer> cartCount = new ObservableField<>();
+    public MutableLiveData<String> cartCount = new MutableLiveData<>();
+
+    public ProductsActivityViewModel() {
+        updateCartProductCount();
+    }
 
     public void openCart(View view){
         view.getContext()
                 .startActivity(new Intent(view.getContext(), CartActivity.class));
+    }
+
+    public void updateCartProductCount(){
+        cartCount.setValue(String.format(Locale.getDefault(),"Cart(%d)", ProdServ.instance().getCartProducts().size()));
     }
 
     public static ProductsActivityViewModel instance() {
