9c2c6375d98174fd6d1afa84c29de2ac6ceaf0ee
pitzzahh <pitzzahh@araopj.tech>
2023-04-02 23:29:50 +0800

    chore: refactor onConfirming checkout

--- app/src/main/java/org/apppuntukan/viewmodel/CheckoutActivityViewModel.java
+++ app/src/main/java/org/apppuntukan/viewmodel/CheckoutActivityViewModel.java
@@ -1,36 +1,30 @@
 package org.apppuntukan.viewmodel;
 
 import android.view.View;
+import android.widget.Toast;
 import android.content.Intent;
 import org.apppuntukan.model.ProdServ;
 import androidx.lifecycle.MutableLiveData;
 import org.apppuntukan.views.ConfirmationActivity;
-import com.google.android.material.snackbar.Snackbar;
 
 public class CheckoutActivityViewModel extends ViewModelBase {
-
     private static CheckoutActivityViewModel deez;
     public final MutableLiveData<String> payable = new MutableLiveData<>();
     public final MutableLiveData<String> amount = new MutableLiveData<>();
+    public MutableLiveData<Boolean> checkEnabled = new MutableLiveData<>();
 
     public CheckoutActivityViewModel() {
-        updateData(new String[] {ProdServ.instance().computeTotal()});
+        updateData(new Object[]{ProdServ.instance().computeTotal()});
     }
 
     public void onConfirmCheckout(View view) { // FIXME: 24/03/2023 might be wrong, don't know how to use.
         String payment = amount.getValue() == null ? "" : amount.getValue();
-        if (payment.matches("\\d|\\d+")) {
-            double total = Double.parseDouble(ProdServ.instance().computeTotal());
-            if (Double.parseDouble(payment) >= total) {
-                ProdServ.instance().setTotal(total);
-                ProdServ.instance().setChange((Double.parseDouble(payment) - total));
-                view.getContext()
-                        .startActivity(new Intent(view.getContext(), ConfirmationActivity.class));
-            } else {
-                Snackbar.make(view, "Insufficient cash", Snackbar.LENGTH_SHORT).show();
-            }
+        double total = Double.parseDouble(ProdServ.instance().computeTotal());
+        if (Double.parseDouble(payment) >= total) {
+            view.getContext()
+                    .startActivity(new Intent(view.getContext(), ConfirmationActivity.class));
         } else {
-            Snackbar.make(view, "Invalid cash", Snackbar.LENGTH_SHORT).show();
+            Toast.makeText(view.getContext(), "Insufficient cash", Toast.LENGTH_SHORT).show();
         }
     }
 
