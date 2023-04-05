02f5ce6d5da1d9a5542c0d6a38405833cdffa958
pitzzahh <pitzzahh@araopj.tech>
2023-04-06 00:29:46 +0800

    chore: partial fix for issue: #14 and #15

--- app/src/main/java/org/apppuntukan/viewmodel/ConfirmationActivityViewModel.java
+++ app/src/main/java/org/apppuntukan/viewmodel/ConfirmationActivityViewModel.java
@@ -1,16 +1,17 @@
 package org.apppuntukan.viewmodel;
 
 import android.view.View;
+import java.util.List;
 import java.util.Objects;
 import android.content.Intent;
 import org.apppuntukan.model.ProdServ;
 import androidx.lifecycle.MutableLiveData;
+import org.apppuntukan.model.Product;
 import org.apppuntukan.views.MainActivity;
 
 public class ConfirmationActivityViewModel extends ViewModelBase {
 
     private static ConfirmationActivityViewModel deez;
-
     public final MutableLiveData<String> payable = new MutableLiveData<>();
     public final MutableLiveData<String> change = new MutableLiveData<>();
 
@@ -21,15 +22,29 @@ public class ConfirmationActivityViewModel extends ViewModelBase {
         });
     }
 
-    public void orderAgain(View view) {
-        for (int i = 0; i < ProdServ.instance().getCartProducts().size(); i++) {
+    public void orderAgain(View view) { // FIXME: 05/04/2023 Fix stocks and cart products
+        List<Product> cartProducts = ProdServ.instance().getCartProducts();
+
+        for (int i = 0; i < cartProducts.size(); i++) {
+            Product product = cartProducts.get(i);
+            product.setStock(product.getStock() - product.getQuantity());
             ProdServ.instance()
                     .getCartProductsRepository()
                     .remove(ProdServ.instance().getCartProducts().get(i));
+            ProdServ.instance()
+                    .getProductRepository()
+                    .update(product);
         }
-        ProdServ.instance()
-                .getCartProducts()
-                .clear();
+
+        int size = ProdServ.instance().getCartProducts().size();
+        CartActivityViewModel.getInstance()
+                .updateData(new Object[]{
+                        size,
+                        ProdServ.instance().computeTotal()
+                });
+        ProductsActivityViewModel.instance()
+                .updateData(new Object[]{size});
+
         view.getContext()
                 .startActivity(new Intent(view.getContext(), MainActivity.class));
     }
